0fa2202d2519e0ea33491dcefe1bd50d6029eb05
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-05-19 23:10:52 +0530

    function to build tree

--- DSA/BinarySearch/binarySearch.js
+++ DSA/BinarySearch/binarySearch.js
@@ -1,10 +1,10 @@
 import { mergeSort, removeDuplicate } from "./arrayOperations.js";
 
 class Node {
-  constructor(data) {
-    this.data = null;
-    this.left = null;
-    this.right = null;
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
   }
 }
 
@@ -12,12 +12,33 @@ export class tree {
   constructor(array) {
     this.array = array;
     this.root = null;
+    this.nodeCount = 0;
   }
 
-  buildTree() {
-    this.array = mergeSort(this.array);
-    this.array = removeDuplicate(this.array);
+  buildTree(array) {
+    array = mergeSort(array);
+    array = removeDuplicate(array);
 
-    console.log(this.array);
+    let startIndex = 0;
+    let endIndex = array.length - 1;
+    if (startIndex > endIndex || array.length === 0) return null;
+
+    let midIndex = Math.floor((startIndex + endIndex) / 2);
+    let leftArray = array.slice(0, midIndex);
+    let rightArray = array.slice(midIndex + 1, array.length);
+
+    let treeNode = new Node(
+      array[midIndex],
+      this.buildTree(leftArray),
+      this.buildTree(rightArray)
+    );
+
+    if (this.nodeCount === 0) {
+      this.root = treeNode;
+    }
+
+    this.nodeCount++;
+
+    return treeNode;
   }
 }
--- DSA/BinarySearch/main.js
+++ DSA/BinarySearch/main.js
@@ -1,4 +1,19 @@
 import { tree } from "./binarySearch.js";
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
 let test = new tree([0, 1, 5, 10, 2, 6, 1, 3, 2, 15]);
-test.buildTree();
+let rootNode = test.buildTree([0, 1, 5, 10, 2, 6, 1, 3, 2, 15]);
+
+prettyPrint(rootNode);
